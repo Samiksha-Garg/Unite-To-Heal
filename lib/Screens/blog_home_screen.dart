497d788a82161027c21db208dcfc895c106c83de
Samiksha Garg <gargsamiksha0306@gmail.com>
2021-07-06 20:33:42 +0530

    developed BlogPage UI

--- lib/Screens/blog_home_screen.dart
+++ lib/Screens/blog_home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:UniteToHeal/constants.dart';
 import 'package:UniteToHeal/CustomWidgets/custom_widgets.dart';
+import 'screens.dart';
 
 class BlogPage extends StatefulWidget {
   const BlogPage({Key? key}) : super(key: key);
@@ -41,39 +42,67 @@ class _BlogPageState extends State<BlogPage> {
       ),
       body: Container(
         width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Stack(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: InputBox(
-                  title: 'Search what you are looking for',
-                  icon: Icons.search,
-                  obscureText: false
-              ),
-            ),
-            SingleChildScrollView(
-              physics: BouncingScrollPhysics(),
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  BlogCard(title: 'All Blogs', color: Colors.yellow,),
-                  BlogCard(title: 'Covid-19 Related',color: Colors.lightGreenAccent,),
-                  BlogCard(title: 'Lifestyle Related',color: Colors.lightBlueAccent,),
-                ],
-              ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                    itemCount: BLOG_DATA.length,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: InputBox(
+                      title: 'Search what you are looking for',
+                      icon: Icons.search,
+                      obscureText: false
+                  ),
+                ),
+                SingleChildScrollView(
+                  physics: BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      BlogCard(title: 'All Blogs', color: Colors.yellow,),
+                      BlogCard(title: 'Covid-19 Related',color: Colors.lightGreenAccent,),
+                      BlogCard(title: 'Lifestyle Related',color: Colors.lightBlueAccent,),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                      itemCount: BLOG_DATA.length,
                       physics: BouncingScrollPhysics(),
                       shrinkWrap: true,
                       itemBuilder: (context,index) {
-                      return BlogList(title: BLOG_DATA[index]["title"], written: BLOG_DATA[index]["written"], image: BLOG_DATA[index]["image"]);
+                        return BlogList(title: BLOG_DATA[index]["title"], written: BLOG_DATA[index]["written"], image: BLOG_DATA[index]["image"]);
                       }),
+                )
+              ],
+            ),
+            Positioned(
+              bottom: 30,
+                left:100,
+                right: 100,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.black
+                  ),
+                  child: Center(
+                    child: TextButton(
+                      child: Text(
+                        'Log In to add blogs \n and better user experience',
+                        style: TextStyle(
+                          color: Colors.white
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, LogInPage.id);
+                      },
+                    ),
+                  ),
+                )
             )
           ],
-        ),
+        )
       ),
     );
   }
